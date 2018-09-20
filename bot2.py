9f2daafa4e66104d2d2552fe45097f46b4a7bde0
Kleymenova Olga <ok.greatteacher@gmail.com>
2018-09-20 14:02:14 +0300

    понадобавляла еще ботиков

--- bot2.py
+++ bot2.py
@@ -1,14 +1,3 @@
-word_dig = {'один':1,
-'два':2,
-'три' :3,
-'четыре':4,
-'пять':5,
-'шесть':6,
-'семь':7,
-'восемь':8,
-'девять':9,
-'десять':10
-}
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
@@ -21,15 +10,33 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     filename='bot.log'
                     )
 
-def greet_user(bot, update):
-    text = 'добро пожаловать в бот, вы нажали /start'
-    logging.info(text)
-    update.message.reply_text(text)
-
-def talk_to_me(bot, update):
+'''def talk_to_me(bot, update):
     user_text = update.message.text 
     print(user_text)
-    update.message.reply_text(user_text)
+    if user_text[-1] == '=':
+        user_text = user_text.split(' ')
+        exp = (user_text[1]).strip('=')
+        for i in range(len(exp)):
+            if exp[i] in ['+', '-', '/', '*']:
+                sign = exp[i]
+                number1 = int(exp[:i])
+                number2 = int(exp[i + 1: ])
+                break
+        if sign == '+':
+            result = number1 + number2
+        elif sign == '-':
+            result = number1 - number2
+        elif sign == '*':
+            result = number1 * number2
+        elif sign == '/':
+            result = number1 / number2
+        print (result)
+        update.message.reply_text(str(result))
+    else:
+        update.message.reply_text(user_text)'''
+
+
+
 
 def calc(bot, update):
     user_text = update.message.text
@@ -57,21 +64,4 @@ def calc(bot, update):
             ans = str(number1 + number2)
         elif user_text[3] =='минус':
             ans = str(number1 - number2)
-        print(ans)
-        update.message.reply_text(str(ans))
-    else:
-        update.message.reply_text(user_text)
-
-def main():
-    mybot = Updater(settings.API_KEY,request_kwargs=settings.PROXY)
-    logging.info('бот запускается, не торопите, ему надо подумать')
-
-
-    dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(CommandHandler("calc", calcm))    
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    mybot.start_polling()
-    mybot.idle()
-
-main()
+    update.message.reply_text(ans)
--- bot3.py
+++ bot3.py
@@ -22,19 +22,22 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     )
 
 def greet_user(bot, update):
-    text = 'добро пожаловать в бот, вы нажали /start'
+    text = 'Вызван /start'
     logging.info(text)
     update.message.reply_text(text)
 
-def talk_to_me(bot, update):
-    user_text = update.message.text 
-    print(user_text)
-    update.message.reply_text(user_text)
+def wc(bot, update):
+    leng = 0
+    if user_text.lower().startswith("'"):
+        user_text = update.message.text.split(' ')
+        leng=len(user_text)-1
+        update.message.reply_text(str(leng))
+    print (leng)
+
 
 def calc(bot, update):
-    user_text = update.message.text
-    ans = user_text
-    exp, _ = user_text.split('=', 1)
+    user_text = update.message.text.split(' ')
+    exp = (user_text[1]).strip('=')
     for i in range(len(exp)):
         if exp[i] in ['+', '-', ':', '*']:
             sign = exp[i]
@@ -49,18 +52,15 @@ def calc(bot, update):
         ans = str(number1 / number2)
     elif sign == '*':
         ans = str(number1 * number2)
-    elif user_text.lower().startwith("сколько будет"):
-        user_text=user_text.split(' ')
-        number1=word_dig[user_text[2]]
-        number2=word_dig[user_text[-1]]
-        if user_text[3] =='плюс':
-            ans = str(number1 + number2)
-        elif user_text[3] =='минус':
-            ans = str(number1 - number2)
-        print(ans)
-        update.message.reply_text(str(ans))
-    else:
-        update.message.reply_text(user_text)
+    print(ans)
+    update.message.reply_text(ans)
+
+    
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
 
 def main():
     mybot = Updater(settings.API_KEY,request_kwargs=settings.PROXY)
@@ -69,7 +69,8 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(CommandHandler("calc", calcm))    
+    dp.add_handler(CommandHandler("wordcount", wc ))
+    dp.add_handler(CommandHandler("calcm", calc))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     mybot.start_polling()
     mybot.idle()
